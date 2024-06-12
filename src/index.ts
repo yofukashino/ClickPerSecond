7fa28af5f7b84955596cb3c8058ab1224ca24d29
Tharki-God <alokender018@gmail.com>
2024-06-12 08:23:52 +0530

    remove useless imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-import { Logger } from "replugged";
-import "./style.css";
-
-export const PluginLogger = Logger.plugin("ClickPerSecond", "#b380ff");
-
 export { default as _CPS } from "./Components/CPS";
